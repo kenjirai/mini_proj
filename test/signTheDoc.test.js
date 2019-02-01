19552f65fcb5df17e2ad1a8a725a60e75a25c699
kenjirai <kenjirai@tuta.io>
2019-02-01 15:13:52 +0545

    signature verification test added

--- test/signTheDoc.test.js
+++ test/signTheDoc.test.js
@@ -5,6 +5,7 @@ const { assertFailure, internalFailWithMsg } = require('./helper/failureUtils');
 const { advanceBlock, latest, latestBlock, increase, increaseTo, duration } = require('./helper/time');
 const getSignData = require('./helper/getSignData');
 const shouldFail = require('./helper/shouldFail');
+const deployDoc = require('./helper/deployDoc');
 
 contract('SignTheDoc', function([accOne, accTwo, accThree, accFour]) {
   beforeEach(async function() {
@@ -16,63 +17,26 @@ contract('SignTheDoc', function([accOne, accTwo, accThree, accFour]) {
     this.expiryDate = this.latestBlockTime.add(duration.days(1));
     this.authorisedSignerList = [accTwo, accFour];
 
-    this.deploy = async function (modify, value) {
-      var expiryDate = this.expiryDate;
-      var signature =  this.signData.signature;
-      var authorisedSignerList =  this.authorisedSignerList;
-      var signData =  this.signData.docHash;
-      var signData = this.signData.r;
-      var s = this.signData.s;
-      var v = this.signData.v;
+    this.deploy = async function(modify, value) {
+      const depData = deployDoc(
+        modify, value, this.expiryDate, this.signData, this.authorisedSignerList
+      );
 
-      if(modify) {
-        switch(expression) {
-          case 'expiryDate':
-            if(!BN.isBN(value)) {
-              throw new Error(`${value} should be web3 BN(bignumber) object`)
-            }
-            expiryDate = value;
-            break;
-
-          case 'signature':
-            signature = value;
-            break;
-
-          case 'authorisedSignerList':
-            authorisedSignerList = value;
-            break;
-
-          case 'signData':
-            signDate = value;
-            break;
-
-          case 's':
-            s = value;
-            break;
-
-          case 'v':
-            v = value;
-            break;
-        }
-      }
-
-      await this.std.createDocToSign(expiryDate,
-        signature,
-        authSignList,
-        docHash,
-        r,
-        s,
-        v);
+      await this.std.createDocToSign(
+        depData.expiryDate,
+        depData.signature,
+        depData.authorisedSignerList,
+        depData.docHash,
+        depData.r,
+        depData.s,
+        depData.v);
     }
   });
 
   describe('CreateDocToSign', function() {
-    //----start tag createDocToSign ---->
-
     describe('successful deployment of the document data', function() {
-      //----successful deployment---->
       beforeEach(async function() {
-        this.deploy();
+        await this.deploy();
         this.docInfo = await this.std.getDocData(this.signData.docHash);
       });
 
@@ -109,79 +73,69 @@ contract('SignTheDoc', function([accOne, accTwo, accThree, accFour]) {
         (new BN(this.docInfo.whoSigned.length))
         .should.be.bignumber.equal(new BN(0));
       });
-      //----end tag successful deployment---->
-    });
+    }); //----end tag successful deployment---->
 
     describe('deployment should fail for incorrect arguments', function() {
-      //----start tag deployment should fail---->
       beforeEach(async function() {
-        this.newExpiryDate = this.latestBlockTime.sub(new BN(10));
+        this.mockExpiryDate = this.latestBlockTime.sub(new BN(10));
         this.mockAccount = await web3.eth.getCoinbase();
         this.mockHash = web3.utils.sha3('yabadabadoooo')
       });
 
-      it('thorws an error for incorrect hash', async function() {
-        await shouldFail(
-          this.std.createDocToSign(
-            this.expiryDate,
-            this.signData.signature,
-            this.authorisedSignerList,
-            this.mockHash,
-            this.signData.r,
-            this.signData.s,
-            this.signData.v
-          )
-        );
-      });
-
       context('incorrect expiry date', function() {
-        //----start tag incorrect expiry date---->
         it('rejects the deployment if the date is already expired', async function() {
-          await shouldFail(
-            this.std.createDocToSign(
-              this.newExpiryDate,
-              this.signData.signature,
-              this.authorisedSignerList,
-              this.signData.docHash,
-              this.signData.r,
-              this.signData.s,
-              this.signData.v
-            )
-          );
+          await shouldFail(this.deploy('expiryDate', this.mockExpiryDate));
         });
 
         it('throws an error if failure not raised', async function() {
-          await assertFailure(shouldFail(
-            this.std.createDocToSign(
-              this.expiryDate,
-              this.signData.signature,
-              this.authorisedSignerList,
-              this.signData.docHash,
-              this.signData.r,
-              this.signData.s,
-              this.signData.v
-            )
-          )
-        );
+          await assertFailure(shouldFail(this.deploy('expiryDate', this.expiryDate)));
         });
 
         it('rejects the deployment with correct message if the date is already expired', async function() {
-          await shouldFail.reverting.withMessage(
-            this.std.createDocToSign(
-              this.newExpiryDate,
-              this.signData.signature,
-              this.authorisedSignerList,
-              this.signData.docHash,
-              this.signData.r,
-              this.signData.s,
-              this.signData.v
-            ),
+          await shouldFail.reverting.withMessage(this.deploy('expiryDate', this.mockExpiryDate),
             'contract expiry date should be greater than current time');
         });
-      });//----end tag incorrect expiry date---->
+      }); //----end tag incorrect expiry date---->
 
-    });//----end tag deployment should fail---->
+      describe('verifySignature', function() {
+        beforeEach(async function() {
+          this.mockHash = web3.utils.sha3('yabadabadoooo')
+          this.mockData = await getSignData(this.mockHash, accTwo)
+          this.errorMsg = "Signature verification failed"
+        });
 
-  });//----end tag createDocToSign ---->
+        it('throws an error for incorrect hash', async function() {
+          await shouldFail.reverting.withMessage(
+            this.deploy('docHash', this.mockHash),
+            this.errorMsg
+          );
+        });
 
-});//===========MAIN TAG END===========>
+        it('throws an error for incorrect r value of ECDSA signature ', async function() {
+          await shouldFail.reverting.withMessage(
+            this.deploy('r', this.mockData.r),
+            this.errorMsg
+          );
+        });
+
+        it('throws an error for incorrect s value of ECDSA signature ', async function() {
+          await shouldFail.reverting.withMessage(
+            this.deploy('s', this.mockData.s),
+            this.errorMsg
+          );
+        });
+
+        it('throws an error for incorrect recovery id, v value ', async function() {
+          await shouldFail.reverting.withMessage(
+            this.deploy('v', this.mockData.v),
+            this.errorMsg
+          );
+        });
+
+      }); //----end tag verifySignature--->
+
+    }); //----end tag deployment should fail---->
+
+  }); //----end tag createDocToSign ---->
+
+}); //===========MAIN TAG END===========>
