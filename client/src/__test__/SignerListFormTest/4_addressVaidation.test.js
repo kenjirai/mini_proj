ff24b9417c46aff0398e2f91a4090760df0ab298
kenjirai <kenjirai@tuta.io>
2019-02-18 19:32:22 +0545

    refacotr address validation test

--- client/src/__test__/SignerListFormTest/4_addressVaidation.test.js
+++ client/src/__test__/SignerListFormTest/4_addressVaidation.test.js
@@ -5,20 +5,21 @@ import SignerListForm from '../../SignerListForm';
 
 describe('AddressValidation', function() {
   let wrapper;
-  let address;
   let mockInput;
   let firstMockInput;
   let secondMockInput;
+  let address;
+
+  const firstInputId = 0;
+  const secondInputId = 1;
 
   beforeEach(() => {
     wrapper = shallow( < SignerListForm / > );
   });
 
   describe('Unique Address', function() {
-    let firstInputId = 0;
-    let secondInputId = 1;
-
     beforeEach(() => {
+
       address = "0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c";
       firstMockInput = {
         value: address,
@@ -59,7 +60,7 @@ describe('AddressValidation', function() {
 
   describe('isAddressValid', function() {
     beforeEach(()=> {
-      let firstInputId = 0;
+      //invalid hex address
       address = "0xJJJ88CE2A7D7d8e725596b9da8b0E5dF4802e3D8";
       mockInput = {
         value: address,
@@ -88,8 +89,6 @@ describe('AddressValidation', function() {
 
   describe('hasSpace', function() {
     beforeEach(()=> {
-      const firstInputId = 0;
-      
       //address contains space
       address = "0xJJ 88CE2A7D7d8e725596b9da8b0E5dF4802e3D8";
       mockInput = {
